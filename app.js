f880e8bf86595ea093e6de9dc6f1fb1fcc56c06e
DarthLuisDa <5cvargas.luis@gmail.com>
2023-11-26 16:51:42 -0600

    Se agrego la descripcion a casi todo el CSS, se agrego descripcion al JS, Fondos, se mejoraron los estilos

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
- // Iniciar objetos vacíos
+ // Iniciar objetos vacíos. Declaracion de Variables. Estructura DOM 20%.
  var tasks = [];
  var taskIndex;
 
- function addTask() {
+ /* Funcion para aniadir Tareas al momento de dar click "Agregar".5% */ 
+  function addTask() {
    // Obtener el valor del input
-   var newTask = document.getElementById("taskInput").value;
+  var newTask = document.getElementById("taskInput").value;
 
    // Validar que el input no esté vacío
    if (newTask !== "") {
-     // Agregar la nueva tarea al arreglo
-     tasks.push(newTask);
 
-     // Guardar el arreglo en Local Storage
-     localStorage.setItem("tasks", JSON.stringify(tasks));
+  // Agregar/Crear la nueva tarea al arreglo 5%
+   tasks.push(newTask);
 
-     // Actualizar la lista de tareas
+ /*  Guardar el arreglo en Local Storage. 
+  Persistir los Datos y almacenarlos en el Local Storage 20%. */
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+     // Actualizar la lista de tareas 
      renderTasks();
 
      // Limpiar el input
      document.getElementById("taskInput").value = "";
    }
  }
-
+/* Funcion para Editar Tareas al momento de dar click "Editar".5%*/ 
  function editTask(index) {
-   // Guardar el índice de la tarea a editar
+   // Guardar el índice de la tarea a editar 
    taskIndex = index;
 
-   // Obtener el valor de la tarea a editar
+   // Obtener el valor de la tarea a editar 
    var task = tasks[index];
 
    // Mostrar el valor en el input de edición
@@ -36,6 +39,7 @@
    document.getElementById("editModal").style.display = "block";
  }
 
+ /* Funcion para Actualizar Tareas al momento de dar click "Actualizar".5% */
  function updateTask() {
    // Obtener el nuevo valor del input de edición
    var updatedTask = document.getElementById("editInput").value;
@@ -53,6 +57,7 @@
    document.getElementById("editModal").style.display = "none";
  }
 
+ /* Funcion para Eliminar Tareas al momento de dar click "Eliminar".5% */
  function deleteTask(index) {
    // Eliminar la tarea del arreglo
    tasks.splice(index, 1);
@@ -63,7 +68,7 @@
    // Actualizar la lista de tareas
    renderTasks();
  }
-
+/* Funcion para renderizar las tareas de la lista */
  function renderTasks() {
    // Obtener el elemento de la lista de tareas
    var taskList = document.getElementById("taskList");
@@ -80,12 +85,14 @@
      listItem.innerText = task;
 
      var editButton = document.createElement("button");
+     editButton.className = 'edit-Button';  /* Se declara el nombre de la clase para conectar CSS */
      editButton.innerText = "Editar";
      editButton.onclick = function() {
        editTask(index);
      };
 
      var deleteButton = document.createElement("button");
+     deleteButton.className = 'delete-Button'; /* Se declara el nombre de la clase para conectar CSS */
      deleteButton.innerText = "Borrar";
      deleteButton.onclick = function() {
        deleteTask(index);
